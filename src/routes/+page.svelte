d9424eafcb35536d532eab5bd8fd42b4f41d8d25
Iwo Plaza <iwoplaza@gmail.com>
2023-10-21 19:48:38 +0200

    Initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta
+		name="description"
+		content="An app helping developers understand the WebGPU memory layout."
+	/>
+</svelte:head>
+
+<section>
+	<h1>WebGPU Memory Layout Inspector</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
